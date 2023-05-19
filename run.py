a1a041b4c73406eb0744e73f5c223a8afb3c0f1c
TemaLaptop <artomkolomeec@gmail.com>
2023-05-19 10:23:32 +0600

    Update

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-from flask_script import Manager
-
 from app import create_app
 from config import DevelopmentConfig
 
 app = create_app(DevelopmentConfig)
-manager = Manager(app)
 
 if __name__ == '__main__':
     app.run()
